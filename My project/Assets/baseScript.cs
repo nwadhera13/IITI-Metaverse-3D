ee12db967e8ee810fba4087f1e459c4cc0ccd107
rajatkushwaha99 <rajatiit5032@gmail.com>
2022-05-24 00:51:30 +0530

    Fixed Jump button

---
+++ My project/Assets/baseScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseScript : MonoBehaviour
+{
+
+    public bool isGrounded = true;
+
+    
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            isGrounded = true;
+        }            
+         
+    }
+
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            isGrounded = false;
+        }
+        
+    }
+
+}
