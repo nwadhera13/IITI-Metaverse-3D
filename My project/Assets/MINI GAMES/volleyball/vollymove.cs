216d45fc03ab520a4a9167805e7ebb79f192c9dc
FRANKENSTEIN0211 <priyanshugrao369@gmail.com>
2022-05-23 20:40:09 +0530

    jump

--- My project/Assets/MINI GAMES/volleyball/vollymove.cs
+++ My project/Assets/MINI GAMES/volleyball/vollymove.cs
@@ -1,35 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class vollymove : MonoBehaviour {
 
 public Vector2 speed = new Vector2(10,10);
 public Rigidbody2D rb;
 private Vector2 movement = new Vector2(1,1);
-
+public Joystick joystick;
 // Use this for initialization
 void Start () {
    rb=GetComponent<Rigidbody2D>();
 }
-
 // Update is called once per frame
 void Update () {
-    float inputX = Input.GetAxis ("Horizontal");
-    float inputY = Input.GetAxis ("Vertical");
-
-    movement = new Vector2(
-        speed.x * inputX,
-        speed.y * inputY);
-        if (Input.GetKeyDown ("space")){
-                         transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
-                 } 
-
-}
+    float inputX = joystick.Horizontal;
+    float inputY = joystick.Vertical;
+    movement = new Vector2(speed.x * inputX,rb.velocity.y);
+    }
 void FixedUpdate()
 {
-    // 5 - Move the game object
     rb.velocity = movement;
-    //rigidbody2D.AddForce(movement);
-
+}
+public void jump(){
+        transform.Translate(Vector3.up * 260 * Time.deltaTime, Space.World);
 }
 }
