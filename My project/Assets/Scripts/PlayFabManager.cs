8f6323d98e4856cf89c9490bdd269662a9938b78
FRANKENSTEIN0211 <priyanshugrao369@gmail.com>
2022-05-25 22:18:23 +0530

    playfab done

---
+++ My project/Assets/Scripts/PlayFabManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+public class PlayFabManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        login();
+    }
+
+    void login(){
+        var request=new LoginWithCustomIDRequest{CustomId=SystemInfo.deviceUniqueIdentifier,CreateAccount=true};
+        PlayFabClientAPI.LoginWithCustomID(request,OnSuccess,OnError);
+    }
+    void OnSuccess(LoginResult result)
+    {
+       Debug.Log("Successful  login/account create");
+    }
+    void OnError(PlayFabError error){
+      Debug.Log("Error while logging in/creating account");
+      Debug.Log(error.GenerateErrorReport());
+    }
+}
